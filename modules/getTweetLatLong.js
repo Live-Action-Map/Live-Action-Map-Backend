08c737465e63217c850ca7a6894b0810f6538d72
Dominika Jadowska <domi.jadowska@gmail.com>
2022-05-31 23:52:55 +0200

    Map actually needs this to be an array

--- modules/getTweetLatLong.js
+++ modules/getTweetLatLong.js
@@ -1,10 +1,6 @@
 module.exports = getLatLong
 
 function getLatLong(data) {
-    let position = {
-        long: "50.40964882790551",
-        lat: "30.519545154411087"
-    }
-
+    let position = ["50.40964882790551","30.519545154411087"]
     return position
 }
