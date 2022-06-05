4cd527035b73b7fe94743e7edc59e1b92c52a54f
Dominika Jadowska <domi.jadowska@gmail.com>
2022-06-05 15:31:00 +0200

    Use promises

--- modules/getTweetLatLong.js
+++ modules/getTweetLatLong.js
@@ -4,42 +4,43 @@ require('dotenv').config()
 
 
 async function getLatLong(data) {
-    let textArray = []
-    let position = []
-    await axios.post(`${process.env.SPACY_URL}/dep`, {
-        "text": data.text,
-        "model": "en_core_web_md",
-        "collapse_punctuation": 0,
-        "collapse_phrases": 1
-    }).then((res) => {
-        textArray = res.data.words
-    }).catch(function (error) {
-        console.log(error.message);
-    });
+    return new Promise(async (resolve, reject) => {
+        let textArray = []
+        let position = []
+        await axios.post(`${process.env.SPACY_URL}/dep`, {
+            "text": data.text,
+            "model": "en_core_web_md",
+            "collapse_punctuation": 0,
+            "collapse_phrases": 1
+        }).then((res) => {
+            textArray = res.data.words
+        }).catch(function (error) {
+            console.log(error.message);
+        });
 
-    textArray.forEach(async (element) => {
-        if (element.tag == "NNP") {
-            let text = element.text.replace('‘', '')
-            text = text.replace("\'", '')
-            text = text.replace("\"", '')
-            let lat = ""
-            let lon = ""
-            await axios.get(`https://nominatim.openstreetmap.org/search.php?q=${text}&format=json`).then((res) => {
-                lat = res.data[0].lat
-                lon = res.data[0].lon
-            }).catch(function (error) {
-                console.log(error.message)
-            });
-            await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`).then((res) => {
-                if (res.data.address.country_code == "ua") {
-                    position.push(lat)
-                    position.push(lon)
-                }
-            }).catch(function (error) {
-                console.log(error.message)
-            });
-        }
-    });
-
-    return position
+        textArray.forEach(async (element) => {
+            if (element.tag == "NNP") {
+                let text = element.text.replace('‘', '')
+                text = text.replace("\'", '')
+                text = text.replace("\"", '')
+                let lat = ""
+                let lon = ""
+                await axios.get(`https://nominatim.openstreetmap.org/search.php?q=${text}&format=json`).then((res) => {
+                    lat = res.data[0].lat
+                    lon = res.data[0].lon
+                }).catch(function (error) {
+                    console.log(error.message)
+                });
+                await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`).then((res) => {
+                    if (res.data.address.country_code == "ua") {
+                        position.push(lat)
+                        position.push(lon)
+                        resolve(position)
+                    }
+                }).catch(function (error) {
+                    console.log(error.message)
+                });
+            }
+        });
+    })
 }
