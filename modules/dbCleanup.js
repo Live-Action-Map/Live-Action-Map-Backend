369d0dd74ab8f2fe90b8ee8747eddd8dfc1ba36a
Dominika Jadowska <domi.jadowska@gmail.com>
2022-07-03 00:49:16 +0200

    Made a typo

--- modules/dbCleanup.js
+++ modules/dbCleanup.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const db = require("./db")
 const cron = require("node-cron")
 async function cleanDB() {
-    let tweets = await db.read("tweets", { created: { $gte: new Date(Date.now() - 24 * 60 * 60 * 1000) } })
+    let tweets = await db.read("tweets", { created: { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000) } })
     tweets.forEach(tweet => {
         db.remove("tweets", { id: tweet.id })
     });
